bf7bebfa5af9eb40f0dbb0159daf18067b36286f
sdelrio <delrio.steven@gmail.com>
2018-03-04 10:36:33 -0600

    Add Routing to Tabs and reactivity to List using a Subscription

--- src/app/star-wars.service.ts
+++ src/app/star-wars.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 
 import { LogService } from "./log.service";
 
@@ -9,6 +10,7 @@ export class StarWarsService {
     { name: 'Darth Vader', side: '' }
   ];
   private logService: LogService;
+  charactersChanged = new Subject<void>();
 
   constructor(logService : LogService){
     this.logService = logService;
@@ -28,7 +30,7 @@ export class StarWarsService {
       return char.name === charInfo.name;
     });
     this.characters[pos].side = charInfo.side;
-
+    this.charactersChanged.next();  //emmiting the event so components can fetch a new copy of the array
     this.logService.writeLog('Changed side of ' + charInfo.name + ', new side: ' + charInfo.side);
   }
 
